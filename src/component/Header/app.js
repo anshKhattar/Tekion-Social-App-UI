9c8443fda799fad63f9ddcb86fe50894997b8fc1
anshKhattar <akhattar@tekion.com>
2023-03-20 11:55:03 +0530

    added tailwind module

---
+++ src/component/Header/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import logo from '../../logo.png';
+
+export default function Header() {
+    return (
+        <>
+            <div className='site-heading h-20 bg-[#01B9A0]'>
+               
+                    <h3 className="text-3xl font-bold pt-4 text-white">Tekion Social App</h3>
+                
+            </div>
+        </>
+    )
+}
