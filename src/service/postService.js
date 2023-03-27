de54dce3c0db6a760eb6c4df5784d75891a27b31
jaman03565 <jaman03565@gmail.com>
2023-03-27 18:19:55 +0530

    refactor

--- src/service/postService.js
+++ src/service/postService.js
@@ -1,11 +1,7 @@
 import API from "./apiService";
 
 const getAllPosts = async () => {
-  const response = await API.get(`/post/`,{
-    headers:{
-      Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbWFuTG92ZXJCb3kiLCJpYXQiOjE2Nzk4OTcwODAsImV4cCI6MTY3OTk4MzQ4MH0.H7oFgrm8V52Br2vnT5p_SMj_KcGO7CBm5xq0zmb4Uks"
-    }
-  });
+  const response = await API.get(`/post/`);
   return response;
 };
 
